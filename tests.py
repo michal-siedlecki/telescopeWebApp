82d0aa78953de3b18c49bac0cc1ebfd52e5e90b5
Michal <siedlecki.michal@gmail.com>
2021-05-03 14:19:53 +0200

    + add calculation test

--- tests.py
+++ tests.py
@@ -1,16 +1,18 @@
+import json
 import unittest
 from app import calculate
 
-expected_result = {'costs': {'a': 5, 'b': 2, 'c': 9, 'd': 7, 'end': 8},
-                   'parents': {'a': 'start', 'b': 'start', 'c': 'a', 'd': 'a', 'end': 'd'}}
-
-
 
 class TestCalculation(unittest.TestCase):
+    expected_result = {'costs': {'a': 5, 'b': 2, 'c': 9, 'd': 7, 'end': 8},
+                       'parents': {'a': 'start', 'b': 'start', 'c': 'a', 'd': 'a', 'end': 'd'}}
+
+    with open("sample_request.json", 'r') as f:
+        request = json.loads(f.read())
 
     def test_calculate(self):
-        result = calculate(request)
-        self.assertEqual(result, str(expected_result), f"Should be \n %s" % expected_result)
+        result = calculate(self.request)
+        self.assertEqual(result, str(self.expected_result), f"Should be \n %s" % self.expected_result)
 
 
 if __name__ == '__main__':
